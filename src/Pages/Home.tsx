161c32ce09f8d4eaea1f578bd57406e15736e51e
acjeff <labaia33@gmail.com>
2025-04-23 15:49:14 +0100

    Carousel

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,14 @@
 import React, {useState, useEffect, useRef} from 'react';
 import '../Styles/Home.scss';
 import HomeLayer from "../Components/HomeLayer";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 import gdImage from '../images/gd/gd1.png';
 
 const projects = [
     {
-        id: 1, name: 'GridDuck', range: '2010 - 2024', items: [
+        id: 1, name: 'GridDuck', range: '2017 - 2025', items: [
             {image: gdImage, description: 'This is an image description'},
             {image: gdImage, description: 'This is an image description'},
             {image: gdImage, description: 'This is an image description'},
@@ -13,20 +16,19 @@ const projects = [
         ]
     },
     {
-        id: 2, name: 'UTC Hub', range: '2010 - 2024', items: [
+        id: 2, name: 'UTC Hub', range: '2016 - 2017', items: [
             {image: gdImage, description: 'This is an image description'}
         ]
     },
     {
-        id: 3, name: 'Gigbloc', range: '2010 - 2024', items: [
+        id: 3, name: 'Gigbloc', range: '2015 - 2016', items: [
             {image: gdImage, description: 'This is an image description'}
         ]
     },
     {
-        id: 4, name: 'WIREWAX', range: '2010 - 2024', items: [
-            {
-                image: gdImage, description: 'This is an image description'
-            }]
+        id: 4, name: 'WIREWAX', range: '2013 - 2015', items: [
+            {image: gdImage, description: 'This is an image description'}
+        ]
     }
 ];
 
@@ -37,6 +39,7 @@ function Home() {
         items: Array<{ image: string, description: string }>;
         id: number, name: string, range: string
     } | null>(null);
+
     const projectsRef = useRef<HTMLDivElement | null>(null);
     const animationRef = useRef<number | null>(null);
     const mouseMoving = useRef(false);
@@ -70,10 +73,7 @@ function Home() {
             const currentWidth = parseFloat(width);
             const target = parseFloat(targetWidth);
             const difference = target - currentWidth;
-
-            // Smooth damping towards target
             const newWidth = currentWidth + difference * 0.1;
-
             setWidth(newWidth.toFixed(2) + '%');
             animationRef.current = requestAnimationFrame(animateWidth);
         };
@@ -90,7 +90,70 @@ function Home() {
     }, [width, targetWidth]);
 
     const col = '#fa6f6f';
-    let colStyle = {color: col};
+    const colStyle = {color: col};
+
+    const projectSliderSettings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToScroll: 1,
+        arrows: true, // or true if you want arrows
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 3000, // large screens
+                settings: {
+                    slidesToShow: 5
+                }
+            }
+        ]
+    };
+
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    };
 
     return (
         <div className={'home-wrapper'} style={{top: showingProject ? 'calc(-100% + 120px)' : '0'}}>
@@ -98,33 +161,47 @@ function Home() {
                 <HomeLayer backgroundCol={col} textCol="white"/>
                 <HomeLayer width={width} backgroundCol="white" textCol={col}/>
             </div>
+
             <div className="projects" ref={projectsRef} style={{bottom: showingProject ? 'calc(100% - 120px)' : '0'}}>
-                {showingProject ? <p className="p-title" style={colStyle}><span className={'clickable'} onClick={() => setShowingProject(null)}>{'^ back '}</span></p> : null}
-                <div className="row">
+                {showingProject ? (
+                    <p className="p-title" style={colStyle}>
+                        <span className={'clickable'} onClick={() => setShowingProject(null)}>
+                            {'^ back '}
+                        </span>
+                    </p>
+                ) : null}
+                {/*<div className="row">*/}
+                <Slider {...projectSliderSettings}>
                     {projects.map((project) => (
                         <div className="project" key={project.id} onClick={() => setShowingProject(project)}>
                             <p style={colStyle}>{project.name}</p>
                             <p className="details" style={colStyle}>{project.range}</p>
                         </div>
                     ))}
-                </div>
+                </Slider>
+                {/*</div>*/}
             </div>
+
             <div className={'project-contents'} style={{top: showingProject ? '120px' : '100%'}}>
                 <div className={'project-content-inner'}>
                     <h1>{showingProject?.name}</h1>
-                    {showingProject && showingProject.items.map((item) => (
-                        <div>
-                            <div style={{
-                                backgroundSize: 'contain',
-                                backgroundRepeat: 'no-repeat',
-                                backgroundPosition: 'center',
-                                width: '100%',
-                                height: '500px',
-                                backgroundImage: `url(${item.image})`
-                            }}/>
-                            <p>{item.description}</p>
-                        </div>
-                    ))}
+                        {/*<Slider {...sliderSettings}>*/}
+                            {showingProject && showingProject.items.map((item, index) => (
+                                <div key={index}>
+                                    <div
+                                        style={{
+                                            backgroundSize: 'contain',
+                                            backgroundRepeat: 'no-repeat',
+                                            backgroundPosition: 'center',
+                                            width: '100%',
+                                            height: '500px',
+                                            backgroundImage: `url(${item.image})`
+                                        }}
+                                    />
+                                    <p style={{textAlign: 'center'}}>{item.description}</p>
+                                </div>
+                            ))}
+                        {/*</Slider>*/}
                 </div>
             </div>
         </div>
