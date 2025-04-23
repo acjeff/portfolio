284bd84977d06f512b6d3745427ebcc064341693
acjeff <labaia33@gmail.com>
2025-04-23 15:51:13 +0100

    Carousel

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -132,29 +132,6 @@ function Home() {
         ]
     };
 
-    const sliderSettings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1
-                }
-            },
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2
-                }
-            }
-        ]
-    };
-
     return (
         <div className={'home-wrapper'} style={{top: showingProject ? 'calc(-100% + 120px)' : '0'}}>
             <div>
