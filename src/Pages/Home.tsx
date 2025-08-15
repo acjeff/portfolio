a6d15fea2412c2c2b83043a0db7d1e58f2e2c275
acjeff <labaia33@gmail.com>
2025-08-15 20:40:17 +0100

    img

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import '../Styles/Home.scss';
 import { performanceMonitor } from '../utils/performance';
 
